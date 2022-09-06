76b2005ef81666f7370e14b8c6bd4ea0cda21524
Christopher Hiller <boneskull@boneskull.com>
2022-09-06 13:16:46 -0700

    feat(appium-tizen-tv-driver): add port flag to rc-pair
    This also fixes an issue where the socket would not be disconnected after token retrieval--you would have to C-c out of it

--- packages/appium-tizen-tv-driver/lib/rc-pair.js
+++ packages/appium-tizen-tv-driver/lib/rc-pair.js
@@ -2,45 +2,70 @@ import yargs from 'yargs/yargs';
 import {TizenRemote, Keys as KEYS} from '@headspinio/tizen-remote';
 import {RC_OPTS} from './driver';
 
-export async function pairRemote({host}) {
-  const rc = new TizenRemote(host, RC_OPTS);
+/**
+ *
+ * @param {{host:string, port?: number}} opts
+ * @returns {Promise<void>}
+ */
+export async function pairRemote({host, port}) {
+  const rc = new TizenRemote(host, {...RC_OPTS, port});
 
-  const token = await rc.getToken();
-  if (token) {
-    console.log(token); // eslint-disable-line no-console
-    return;
+  try {
+    const token = await rc.getToken({force: true});
+    if (token) {
+      console.log(token); // eslint-disable-line no-console
+      return;
+    }
+    throw new Error(`Could not retrieve token; please try allowing the remote again`);
+  } finally {
+    await rc.disconnect();
   }
-
-  throw new Error(`Could not retrieve token; please try allowing the remote again`);
 }
 
-export async function testRemote({host, token}) {
+/**
+ *
+ * @param {{host: string, token:string, port?: number}} opts
+ */
+export async function testRemote({host, token, port}) {
   const rc = new TizenRemote(host, {
     ...RC_OPTS,
+    port,
     token,
   });
+  try {
+    await rc.click(KEYS.HOME);
+    console.log('OK'); // eslint-disable-line no-console
+  } finally {
+    await rc.disconnect();
+  }
 
-  await rc.click(KEYS.HOME);
-  await rc.disconnect();
 }
 
 async function main() {
-  const {host, token} = yargs(process.argv.slice(2))
+  const {host, token, port} = await yargs(process.argv.slice(2))
     .option('host', {
+      type: 'string',
       alias: 'h',
       describe: 'IP address of the Tizen device',
     })
+    .option('port', {
+      type: 'number',
+      alias: 'p',
+      describe: 'Port of websocket server on the Tizen device',
+      default: 8002
+    })
     .option('token', {
+      type: 'string',
       alias: 't',
       describe: 'Token retrieved from previous call (to test if pairing is successful)',
     })
     .demandOption(['host'], 'Please provide "host" argument')
-    .help().argv;
+    .help().parse();
 
   if (token) {
-    await testRemote({host, token});
+    await testRemote({host, token, port});
   } else {
-    await pairRemote({host});
+    await pairRemote({host, port});
   }
 }
 
