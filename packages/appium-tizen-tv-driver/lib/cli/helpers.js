0ed31c600d34a2f559d413b37bda40710d006fb9
Christopher Hiller <boneskull@boneskull.com>
2022-09-07 16:44:58 -0700

    chore(appium-tizen-tv-driver): update types for cli tool modules

--- packages/appium-tizen-tv-driver/lib/cli/helpers.js
+++ packages/appium-tizen-tv-driver/lib/cli/helpers.js
@@ -6,44 +6,76 @@ import log from '../logger';
 const TIZEN_BIN_NAME = 'tizen';
 const SDB_BIN_NAME = 'sdb';
 
-const BIN_PATHS = {
+/**
+ * Lookup of path parts by bin name, relative to `TIZEN_HOME` env var
+ */
+const BIN_PATHS = Object.freeze({
   [TIZEN_BIN_NAME]: ['tools', 'ide', 'bin', 'tizen'],
   [SDB_BIN_NAME]: ['tools', 'sdb'],
-};
+});
+
+/**
+ * In-memory cache of known executable paths
+ * @type {Record<string,string>}
+ */
 const bins = {};
 
+/**
+ * Runs external command by "bin name"
+ * @param {KnownBinName} bin
+ * @param {string[]} args
+ */
 async function runCmd(bin, args) {
-  if (!bins[bin]) {
+  if (!(bin in bins)) {
     await setBin(bin);
   }
   log.info(`Running command: ${bins[bin]} ${args.join(' ')}`);
   try {
     return await exec(bins[bin], args);
-  } catch (e) {
-    const stdout = e.stdout.replace(/[\r\n]+/, ' ');
-    const stderr = e.stderr.replace(/[\r\n]+/, ' ');
+  } catch (err) {
+    const e = /** @type {import('teen_process').ExecError} */(err);
+    const stdout = e.stdout.replace(/[\r\n]+/g, ' ');
+    const stderr = e.stderr.replace(/[\r\n]+/g, ' ');
     e.message = `${e.message}. Stdout was: '${stdout}'. Stderr was: '${stderr}'`;
     throw e;
   }
 }
 
+/**
+ * Type guard
+ * @param {any} value
+ * @returns {value is KnownBinName}
+ */
+function isKnownBinName(value) {
+  return value === TIZEN_BIN_NAME || value === SDB_BIN_NAME;
+}
+
+/**
+ * @param {string} name
+ */
 async function setBin(name) {
-  if (!Object.keys(BIN_PATHS).includes(name)) {
+  if (!isKnownBinName(name)) {
     throw new Error(`We don't know how to find the '${name}' binary`);
   }
   log.info(`Attempting to verify location of the '${name}' binary`);
   if (!process.env.TIZEN_HOME) {
-    throw new Error(`TIZEN_HOME env var must be set so that we can find binary`);
+    throw new Error(`TIZEN_HOME env var must be set so that we can find Tizen CLI tools`);
   }
   // TODO check name of binary on windows and update based on platform if necessary
   const bin = path.resolve(process.env.TIZEN_HOME, ...BIN_PATHS[name]);
-  if (!(await fs.exists(bin))) {
+  try {
+    await fs.access(bin, fs.constants.R_OK | fs.constants.X_OK);
+  } catch {
     throw new Error(
       `Tried to find binary at ${bin} but it did not exist or was not ` +
-        `accessible. Please double-check permissions and TIZEN_HOME value`
+        `executable. Please double-check permissions and TIZEN_HOME value`
     );
   }
   bins[name] = bin;
   log.info(`Binary was found at ${bin}`);
 }
 export {runCmd, setBin, TIZEN_BIN_NAME, SDB_BIN_NAME};
+
+/**
+ * @typedef {TIZEN_BIN_NAME|SDB_BIN_NAME} KnownBinName
+ */
--- packages/appium-tizen-tv-driver/lib/cli/sdb.js
+++ packages/appium-tizen-tv-driver/lib/cli/sdb.js
@@ -4,26 +4,41 @@ import {runCmd, SDB_BIN_NAME} from './helpers';
 const DEBUG_PORT_RE = /^(?:.*port:\s)(?<port>\d{1,5})$/;
 const APP_LIST_RE = /^[^']*'(?<name>[^']+)'[^']+'(?<id>[^']+)'.*$/;
 
+/**
+ *
+ * @param {string?} udid
+ * @param {string[]} args
+ */
 async function runSDBCmd(udid, args) {
   const restriction = udid ? ['-s', udid] : [];
   return await runCmd(SDB_BIN_NAME, [...restriction, ...args]);
 }
 
+/**
+ * @param {Pick<TizenTVDriverCaps, 'appPackage' | 'udid'>} caps
+ */
 async function debugApp({appPackage, udid}) {
   log.info(`Starting ${appPackage} in debug mode on ${udid}`);
   const {stdout} = await runSDBCmd(udid, ['shell', '0', 'debug', appPackage]);
   try {
-    const port = stdout.trim().match(DEBUG_PORT_RE).groups.port;
+    const port = stdout.trim().match(DEBUG_PORT_RE)?.groups?.port;
+    if (!port) {
+      throw new Error(`Cannot parse debug port from sdb output`);
+    }
     log.info(`Debug port opened on ${port}`);
     return port;
   } catch (e) {
+    const message = /** @type {import('teen_process').ExecError} */(e);
     throw new Error(
       `Unable to retrieve debugger port from debug start invocation. ` +
-        `Original error: ${e.message}`
+        `Original error: ${message}`
     );
   }
 }
 
+/**
+ * @param {Pick<TizenTVDriverCaps, 'udid'>} caps
+ */
 async function listApps({udid}) {
   log.info(`Listing apps installed on '${udid}'`);
   const {stdout} = await runSDBCmd(udid, ['shell', '0', 'applist']);
@@ -32,7 +47,7 @@ async function listApps({udid}) {
     .map((line) => {
       // TODO WIP fix this regex
       const match = line.match(APP_LIST_RE);
-      if (!match) {
+      if (!match?.groups) {
         return false;
       }
       return {appName: match.groups.name, appPackage: match.groups.id};
@@ -42,26 +57,41 @@ async function listApps({udid}) {
   return apps;
 }
 
+/**
+ * @param {Pick<TizenTVDriverCaps, 'udid'|'localPort'|'remotePort'>} caps
+ */
 async function forwardPort({udid, localPort, remotePort}) {
   log.info(`Forwarding local TCP port ${localPort} to port ${remotePort} on device ${udid}`);
   await runSDBCmd(udid, ['forward', `tcp:${localPort}`, `tcp:${remotePort}`]);
 }
 
+/**
+ * @param {Pick<TizenTVDriverCaps, 'udid'|'localPort'>} caps
+ */
 async function removeForwardedPort({udid, localPort}) {
   log.info(`Removing port forwarding for device ${udid} and local port ${localPort}`);
   await runSDBCmd(udid, ['forward', '--remove', localPort]);
 }
 
+/**
+ * @param {Pick<TizenTVDriverCaps, 'udid'>} caps
+ */
 async function removeForwardedPorts({udid}) {
   log.info(`Removing all port forwarding for device ${udid}`);
   await runSDBCmd(udid, ['forward', '--remove-all']);
 }
 
+/**
+ * @param {Pick<TizenTVDriverCaps, 'udid'>} caps
+ */
 async function connectDevice({udid}) {
   log.info(`Connecting device '${udid}' via sdb`);
   await runSDBCmd(null, ['connect', udid]);
 }
 
+/**
+ * @param {Pick<TizenTVDriverCaps, 'udid'>} caps
+ */
 async function disconnectDevice({udid}) {
   log.info(`Disconnecting device '${udid}' via sdb`);
   await runSDBCmd(null, ['disconnect', udid]);
@@ -77,3 +107,7 @@ export {
   disconnectDevice,
   removeForwardedPort,
 };
+
+/**
+ * @typedef {import('../driver').TizenTVDriverCaps} TizenTVDriverCaps
+ */
