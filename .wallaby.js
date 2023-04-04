0059c27498a88a62af22bcd4a8de4b716c8d144b
Christopher Hiller <boneskull@boneskull.com>
2023-04-04 13:52:03 -0700

    chore: build all with typescript
    This change rips out all Babel config and retools it to use TypeScript and Appium's ESLint config for TS.

--- .wallaby.js
+++ .wallaby.js
@@ -3,9 +3,14 @@
 module.exports = (wallaby) => {
   return {
     compilers: {
-      '**/*.js': wallaby.compilers.babel(),
+      '**/*.js': wallaby.compilers.typeScript({
+        allowJs: true,
+        allowSyntheticDefaultImports: true,
+        resolveJsonModule: true,
+        isolatedModules: true,
+      }),
+      '**/*.ts?(x)': wallaby.compilers.typeScript(),
     },
-    debug: true,
     env: {
       type: 'node',
       params: {
@@ -15,20 +20,20 @@ module.exports = (wallaby) => {
     files: [
       './packages/*/*.js',
       './packages/*/lib/**/*.js',
+      './packages/*/lib/**/*.ts',
       './packages/*/*.json',
       '!./packages/*/build/**',
       '!./packages/*/node_modules/**',
-      '!./packages/*/gulpfile.js',
       '!./packages/*/scripts/**',
       './packages/*/test/**/fixtures/**/*',
       './packages/*/test/**/helpers.js',
-      './babel.config.json',
       './packages/tizen-remote/test/e2e/server.js',
       './packages/appium-tizen-tv-driver/test/unit/mocks.js'
     ],
     testFramework: 'mocha',
     tests: [
       './packages/*/test/unit/**/*.spec.js',
+      './packages/*/test/unit/**/*.spec.ts',
       './packages/tizen-remote/test/e2e/**/*.e2e.spec.js'
     ],
     runMode: 'onsave',
